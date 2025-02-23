b9f1e12f0c3b12a131eb532b682e627ef0ac42c6
PJutch <103033969+PJutch@users.noreply.github.com>
2025-02-23 20:07:51 +0300

    Rewrite UserController for Spring

---
+++ app/src/main/java/org/hsse/news/api/controllers/SpringUserController.java
@@ -0,0 +1,122 @@
+package org.hsse.news.api.controllers;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import lombok.extern.slf4j.Slf4j;
+import org.hsse.news.api.authorizers.Authorizer;
+import org.hsse.news.api.schemas.request.user.UserPasswordChangeRequest;
+import org.hsse.news.api.schemas.request.user.UserRegisterRequest;
+import org.hsse.news.api.schemas.shared.UserInfo;
+import org.hsse.news.api.util.ControllerUtil;
+import org.hsse.news.database.user.UserService;
+import org.hsse.news.database.user.exceptions.EmailConflictException;
+import org.hsse.news.database.user.exceptions.InvalidCurrentPasswordException;
+import org.hsse.news.database.user.exceptions.SameNewPasswordException;
+import org.hsse.news.database.user.models.User;
+import org.hsse.news.database.user.models.UserId;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import spark.Response;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/users")
+@Slf4j
+public class SpringUserController {
+    private final UserService userService;
+    private final Authorizer authorizer;
+
+    public SpringUserController(final UserService userService, final Authorizer authorizer) {
+        this.userService = userService;
+        this.authorizer = authorizer;
+    }
+
+    @PostMapping("/register")
+    ResponseEntity<Void> register(@RequestBody UserRegisterRequest request) {
+        ControllerUtil.logRequest(request, path);
+
+        try {
+            final User user = userService.register(
+                    new User(request.email(), request.password(), request.username()));
+            log.debug("Registered user with id = {}", user.id());
+            return ResponseEntity.created(ServletUriComponentsBuilder.fromCurrentServletMapping()
+                    .path("/{id}").buildAndExpand(user.id()).toUri()).build();
+        } catch (EmailConflictException e) {
+            return processEmailConflict(e);
+        }
+    }
+
+    @GetMapping
+    private ResponseEntity<UserInfo> get() {
+        ControllerUtil.logRequest(request, path);
+
+        final UserId userId = authorizer.authorizeStrict(request);
+
+        final Optional<User> userOptional = userService.findById(userId);
+        if (userOptional.isEmpty()) {
+            log.warn("User not found for id = {}", userId);
+            return ResponseEntity.notFound().build();
+        }
+
+        log.debug("Successfully found user by id = {}", userOptional.get().id());
+
+        return ResponseEntity.ok(new UserInfo(
+                userOptional.get().email(),
+                userOptional.get().username()
+        ));
+    }
+
+    @PutMapping
+    private ResponseEntity<Void> update(@RequestBody UserInfo userInfo) {
+        ControllerUtil.logRequest(request, path);
+
+        final UserId userId = authorizer.authorizeStrict(request);
+
+        try {
+            userService.update(userId, userInfo.email(), userInfo.username());
+
+            log.debug("Successfully updated user with id = {}", userId);
+            return ResponseEntity.noContent().build();
+        } catch (EmailConflictException e) {
+            return processEmailConflict(e);
+        }
+    }
+
+    @PutMapping("/password")
+    private ResponseEntity<String> changePassword(@RequestBody UserPasswordChangeRequest request) {
+        ControllerUtil.logRequest(request, path);
+
+        final UserId userId = authorizer.authorizeStrict(request);
+
+        try {
+            userService.updatePassword(userId, request.currentPassword(), request.newPassword());
+
+            log.debug("Successfully updated password for user with id = {}", userId);
+            return ResponseEntity.noContent().build();
+        } catch (SameNewPasswordException e) {
+            log.debug("Same new password for user with id = {}", userId);
+            return ResponseEntity.status(HttpStatus.ALREADY_REPORTED)
+                    .body("Valid current password, new password matches it");
+        } catch (InvalidCurrentPasswordException e) {
+            log.debug("Invalid current password for user with id = {}", userId);
+            return ResponseEntity.status(HttpStatus.PRECONDITION_FAILED)
+                    .body("Invalid current password");
+        }
+    }
+
+    private ResponseEntity<String> processEmailConflict(
+            final EmailConflictException e, final Response response
+    ) throws JsonProcessingException {
+        log.debug("Email conflict: {}", e.getMessage());
+
+        response.status(409);
+        return ResponseEntity.status(HttpStatus.CONFLICT).body("Email already exists");
+    }
+}
