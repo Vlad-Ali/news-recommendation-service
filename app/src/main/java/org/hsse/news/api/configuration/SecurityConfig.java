e05b82db9866c8106b16458e92b1cebef4ff6ad8
PJutch <103033969+PJutch@users.noreply.github.com>
2025-02-23 21:03:38 +0300

    Create a SecurityConfig

---
+++ app/src/main/java/org/hsse/news/api/configuration/SecurityConfig.java
@@ -0,0 +1,28 @@
+package org.hsse.news.api.configuration;
+
+import org.hsse.news.api.filters.JwtTokenFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    private final JwtTokenFilter jwtTokenFilter;
+
+    public SecurityConfig(JwtTokenFilter jwtTokenFilter) {
+        this.jwtTokenFilter = jwtTokenFilter;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class)
+                .authorizeHttpRequests(authorizeHttpRequests ->
+                        authorizeHttpRequests.requestMatchers("/**").permitAll());
+        return http.build();
+    }
+}
