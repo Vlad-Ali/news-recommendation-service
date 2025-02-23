b04c811bf9765dddf575c7988b863a601113ffac
PJutch <103033969+PJutch@users.noreply.github.com>
2025-02-23 20:59:01 +0300

    Add Spring Security

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,8 @@ dependencies {
 
     implementation("org.springframework.boot:spring-boot-starter-web")
 
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.security:spring-security-test")
     implementation("io.jsonwebtoken:jjwt:0.12.3")
 
     implementation("org.projectlombok:lombok")
