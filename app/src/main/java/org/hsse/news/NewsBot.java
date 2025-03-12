b8580dd4e917ddf39f05044e34b0ddd4e3e39eb8
PJutch <103033969+PJutch@users.noreply.github.com>
2025-03-12 23:18:04 +0300

    Rewrite to telegrambots bcs its simpler, and I wasn't using abilitybots features

--- app/src/main/java/org/hsse/news/NewsBot.java
+++ app/src/main/java/org/hsse/news/NewsBot.java
@@ -3,63 +3,77 @@ package org.hsse.news;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
-import org.telegram.abilitybots.api.bot.AbilityBot;
-import org.telegram.abilitybots.api.objects.Ability;
-import org.telegram.abilitybots.api.objects.Flag;
-import org.telegram.abilitybots.api.objects.Reply;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
-import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboard;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import static org.telegram.abilitybots.api.objects.Locality.USER;
-import static org.telegram.abilitybots.api.objects.Privacy.PUBLIC;
-import static org.telegram.abilitybots.api.util.AbilityUtils.getChatId;
-
 @Component
-public class NewsBot extends AbilityBot {
+public class NewsBot extends TelegramLongPollingBot {
     enum ChatState {
         NO_POST,
         POST,
         AWAITING_URI
     }
 
-    private final Map<Long, ChatState> chatStates;
+    private final Map<Long, ChatState> chatStates = new HashMap<>();
 
     @Autowired
     public NewsBot(Environment environment) {
-        super(environment.getProperty("bot-token"), "HsseNewsTeam1Bot");
-        chatStates = db.getMap("chatStates");
+        super(environment.getProperty("bot-token"));
     }
 
     @Override
-    public long creatorId() {
-        return 1;
+    public void onUpdateReceived(Update update) {
+        try {
+            long chatId = update.getMessage().getChatId();
+            String text = update.getMessage().getText();
+            ChatState state = chatStates.get(chatId);
+
+            if ("/stop".equalsIgnoreCase(text)) {
+                chatStates.remove(chatId);
+            } else if (state == null || "/start".equalsIgnoreCase(text)) {
+                replyToStarted(chatId);
+            } else if (state.equals(ChatState.AWAITING_URI)) {
+                replyToAwaitingUri(chatId, text);
+            } else if (state.equals(ChatState.POST) && "like".equalsIgnoreCase(text)) {
+                sendLikeRecorded(chatId);
+            } else if (state.equals(ChatState.POST) && "dislike".equalsIgnoreCase(text)) {
+                sendDislikeRecorded(chatId);
+            } else if ("next".equalsIgnoreCase(text)) {
+                sendPost(chatId);
+            } else if ("add source".equalsIgnoreCase(text)) {
+                sendAddSource(chatId);
+            } else {
+                sendUnsupported(chatId, text);
+            }
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    public Ability startBot() {
-        return Ability
-                .builder()
-                .name("start")
-                .info("Starts working with a bot")
-                .locality(USER)
-                .privacy(PUBLIC)
-                .action(context -> {
-                    SendMessage message = new SendMessage();
-                    message.setChatId(context.chatId());
+    @Override
+    public String getBotUsername() {
+        return "HsseNewsTeam1Bot";
+    }
 
-                    message.setText("Welcome!");
-                    message.setReplyMarkup(getNoPostKeyboard());
+    private void replyToStarted(long chatId) throws TelegramApiException {
+        SendMessage message = new SendMessage();
+        message.setChatId(chatId);
 
-                    silent.execute(message);
+        message.setText("Welcome!");
+        message.setReplyMarkup(getNoPostKeyboard());
 
-                    chatStates.put(context.chatId(), ChatState.NO_POST);
-                })
-                .build();
+        execute(message);
+
+        chatStates.put(chatId, ChatState.NO_POST);
     }
 
     private static KeyboardRow getCommandsRow() {
@@ -80,119 +94,76 @@ public class NewsBot extends AbilityBot {
         return new ReplyKeyboardMarkup(List.of(getCommandsRow()));
     }
 
-    private void sendPost(long chatId) {
+    private void sendPost(long chatId) throws TelegramApiException {
         SendMessage sendMessage = new SendMessage();
         sendMessage.setChatId(chatId);
 
         sendMessage.setText("Предстааавьте, что это пост с новостями");
         sendMessage.setReplyMarkup(getPostKeyboard());
 
-        silent.execute(sendMessage);
+        execute(sendMessage);
 
         chatStates.put(chatId, ChatState.POST);
     }
 
-    private void sendLikeRecorded(long chatId) {
+    private void sendLikeRecorded(long chatId) throws TelegramApiException {
         SendMessage sendMessage = new SendMessage();
         sendMessage.setChatId(chatId);
 
         sendMessage.setText("Предстааавьте, что мы записал лайк");
         sendMessage.setReplyMarkup(getNoPostKeyboard());
 
-        silent.execute(sendMessage);
+        execute(sendMessage);
 
         chatStates.put(chatId, ChatState.NO_POST);
     }
 
-    private void sendDislikeRecorded(long chatId) {
+    private void sendDislikeRecorded(long chatId) throws TelegramApiException {
         SendMessage sendMessage = new SendMessage();
         sendMessage.setChatId(chatId);
 
         sendMessage.setText("Предстааавьте, что мы записал дизлайк");
         sendMessage.setReplyMarkup(getNoPostKeyboard());
 
-        silent.execute(sendMessage);
+        execute(sendMessage);
 
         chatStates.put(chatId, ChatState.NO_POST);
     }
 
-    private void sendAddSource(long chatId) {
+    private void sendAddSource(long chatId) throws TelegramApiException {
         SendMessage sendMessage = new SendMessage();
         sendMessage.setChatId(chatId);
 
         sendMessage.setText("Введите URI нового источника: ");
 
-        silent.execute(sendMessage);
+        execute(sendMessage);
 
         chatStates.put(chatId, ChatState.AWAITING_URI);
     }
 
-    private void sendUnsupported(long chatId, Message operation) {
+    private void sendUnsupported(long chatId, String operation) throws TelegramApiException {
         SendMessage sendMessage = new SendMessage();
         sendMessage.setChatId(chatId);
 
-        sendMessage.setText("Операция " + operation.getText() + " не поддерживается");
+        sendMessage.setText("Операция " + operation + " не поддерживается");
         if (chatStates.get(chatId).equals(ChatState.POST)) {
             sendMessage.setReplyMarkup(getPostKeyboard());
         } else {
             sendMessage.setReplyMarkup(getNoPostKeyboard());
         }
 
-        silent.execute(sendMessage);
+        execute(sendMessage);
     }
 
-    private void replyToStarted(long chatId, Message message) {
-        if ("next".equalsIgnoreCase(message.getText())) {
-            sendPost(chatId);
-        } else if ("add source".equalsIgnoreCase(message.getText())) {
-            sendAddSource(chatId);
-        } else {
-            sendUnsupported(chatId, message);
-        }
-    }
-
-    private void replyToScrolling(long chatId, Message message) {
-        if ("next".equalsIgnoreCase(message.getText())) {
-            sendPost(chatId);
-        } else if ("like".equalsIgnoreCase(message.getText())) {
-            sendLikeRecorded(chatId);
-        } else if ("dislike".equalsIgnoreCase(message.getText())) {
-            sendDislikeRecorded(chatId);
-        } else if ("add source".equalsIgnoreCase(message.getText())) {
-            sendAddSource(chatId);
-        } else {
-            sendUnsupported(chatId, message);
-        }
-    }
-
-    private void replyToAwaitingUri(long chatId, Message message) {
+    private void replyToAwaitingUri(long chatId, String message) throws TelegramApiException {
         SendMessage sendMessage = new SendMessage();
         sendMessage.setChatId(chatId);
 
-        sendMessage.setText("Источник " + message.getText() + " добавлен");
+        sendMessage.setText("Источник " + message + " добавлен");
         sendMessage.setReplyMarkup(getNoPostKeyboard());
 
-        silent.execute(sendMessage);
+        execute(sendMessage);
 
         chatStates.put(chatId, ChatState.NO_POST);
-    }
-
-    private void replyToButtons(long chatId, Message message) {
-        if ("/stop".equalsIgnoreCase(message.getText())) {
-            chatStates.remove(chatId);
-            return;
-        }
-
-        switch (chatStates.get(chatId)) {
-            case NO_POST -> replyToStarted(chatId, message);
-            case POST -> replyToScrolling(chatId, message);
-            case AWAITING_URI -> replyToAwaitingUri(chatId, message);
-            default -> throw new IllegalStateException("Unexpected chat state");
-        }
-    }
-
-    public Reply replyToButtons() {
-        return Reply.of((bot, update) -> replyToButtons(getChatId(update), update.getMessage()),
-                Flag.TEXT, update -> chatStates.containsKey(getChatId(update)));
     }
 }
