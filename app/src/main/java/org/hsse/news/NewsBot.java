d45a0738c46416453b8b27df76f6aff659552d50
PJutch <103033969+PJutch@users.noreply.github.com>
2025-03-13 00:10:57 +0300

    Refactor more, keep track of a post id and like/dislike status

--- app/src/main/java/org/hsse/news/NewsBot.java
+++ app/src/main/java/org/hsse/news/NewsBot.java
@@ -14,13 +14,36 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 @Component
 public class NewsBot extends TelegramLongPollingBot {
-    enum ChatState {
-        NO_POST,
-        POST,
-        AWAITING_URI
+    private sealed interface ChatState {
+    }
+
+    private record NormalChatState(Optional<Long> post, Optional<Boolean> liked) implements ChatState {
+        public NormalChatState() {
+            this(Optional.empty(), Optional.empty());
+        }
+
+        public NormalChatState(long post) {
+            this(Optional.of(post), Optional.empty());
+        }
+
+        public NormalChatState(long post, boolean liked) {
+            this(Optional.of(post), Optional.of(liked));
+        }
+
+        boolean isLiked() {
+            return liked.equals(Optional.of(true));
+        }
+
+        boolean isDisliked() {
+            return liked.equals(Optional.of(false));
+        }
+    }
+
+    private static final class AwaitingUriChatState implements ChatState {
     }
 
     private final Map<Long, ChatState> chatStates = new HashMap<>();
@@ -28,6 +51,104 @@ public class NewsBot extends TelegramLongPollingBot {
     @Autowired
     public NewsBot(Environment environment) {
         super(environment.getProperty("bot-token"));
+    }
+
+    @Override
+    public String getBotUsername() {
+        return "HsseNewsTeam1Bot";
+    }
+
+    private static KeyboardRow getNoPostCommandsRow() {
+        KeyboardRow commandsRow = new KeyboardRow();
+        commandsRow.add("next");
+        commandsRow.add("add source");
+        return commandsRow;
+    }
+
+    private ReplyKeyboard getKeyboard(ChatState state) {
+        if (!(state instanceof NormalChatState normalChatState)) {
+            return null;
+        }
+
+        if (normalChatState.post().isEmpty()) {
+            return new ReplyKeyboardMarkup(List.of(getNoPostCommandsRow()));
+        } else if (normalChatState.isLiked()) {
+            KeyboardRow likeRow = new KeyboardRow();
+            likeRow.add("unlike");
+            likeRow.add("dislike");
+            return new ReplyKeyboardMarkup(List.of(likeRow, getNoPostCommandsRow()));
+        } else if (normalChatState.isDisliked()) {
+            KeyboardRow likeRow = new KeyboardRow();
+            likeRow.add("like");
+            likeRow.add("undislike");
+            return new ReplyKeyboardMarkup(List.of(likeRow, getNoPostCommandsRow()));
+        } else {
+            KeyboardRow likeRow = new KeyboardRow();
+            likeRow.add("like");
+            likeRow.add("dislike");
+            return new ReplyKeyboardMarkup(List.of(likeRow, getNoPostCommandsRow()));
+        }
+    }
+
+    private void sendMessage(long chatId, String text) throws TelegramApiException {
+        SendMessage message = new SendMessage();
+        message.setChatId(chatId);
+
+        message.setText(text);
+        message.setReplyMarkup(getKeyboard(chatStates.get(chatId)));
+
+        execute(message);
+    }
+
+    private boolean handleLikes(long chatId, NormalChatState state, String text)
+            throws TelegramApiException {
+        if (state.post().isEmpty()) {
+            return false;
+        }
+        long postId = state.post().get();
+
+        if (!state.isLiked() && "like".equalsIgnoreCase(text)) {
+            chatStates.put(chatId, new NormalChatState(postId, true));
+            sendMessage(chatId, "Предстааавьте, что мы записали лайк для поста " + postId);
+            return true;
+        } else if (state.isLiked() && "unlike".equalsIgnoreCase(text)) {
+            chatStates.put(chatId, new NormalChatState(postId));
+            sendMessage(chatId, "Предстааавьте, что мы убрали лайк для поста " + postId);
+            return true;
+        } else if (!state.isDisliked() && "dislike".equalsIgnoreCase(text)) {
+            chatStates.put(chatId, new NormalChatState(postId, false));
+            sendMessage(chatId, "Предстааавьте, что мы записал дизлайк для поста " + postId);
+            return true;
+        } else if (state.isDisliked() && "undislike".equalsIgnoreCase(text)) {
+            chatStates.put(chatId, new NormalChatState(postId));
+            sendMessage(chatId, "Предстааавьте, что мы убрали дизлайк для поста " + postId);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private boolean handleNormalState(long chatId, ChatState state, String text)
+            throws TelegramApiException {
+        if (!(state instanceof NormalChatState)) {
+            return false;
+        }
+
+        if (handleLikes(chatId, (NormalChatState) state, text)) {
+            return true;
+        }
+
+        if ("next".equalsIgnoreCase(text)) {
+            long postId = (long) (Math.random() * 1000);
+            chatStates.put(chatId, new NormalChatState(postId));
+            sendMessage(chatId, "Предстааавьте, что это пост " + postId + " с новостями");
+            return true;
+        } else if ("add source".equalsIgnoreCase(text)) {
+            chatStates.put(chatId, new AwaitingUriChatState());
+            sendMessage(chatId, "Введите URI нового источника: ");
+            return true;
+        }
+        return false;
     }
 
     @Override
@@ -40,66 +161,16 @@ public class NewsBot extends TelegramLongPollingBot {
             if ("/stop".equalsIgnoreCase(text)) {
                 chatStates.remove(chatId);
             } else if (state == null || "/start".equalsIgnoreCase(text)) {
-                chatStates.put(chatId, ChatState.NO_POST);
+                chatStates.put(chatId, new NormalChatState());
                 sendMessage(chatId, "Welcome!");
-            } else if (state.equals(ChatState.AWAITING_URI)) {
-                chatStates.put(chatId, ChatState.NO_POST);
+            } else if (state instanceof AwaitingUriChatState) {
+                chatStates.put(chatId, new NormalChatState());
                 sendMessage(chatId, "Источник " + text + " добавлен");
-            } else if (state.equals(ChatState.POST) && "like".equalsIgnoreCase(text)) {
-                chatStates.put(chatId, ChatState.NO_POST);
-                sendMessage(chatId, "Предстааавьте, что мы записал лайк");
-            } else if (state.equals(ChatState.POST) && "dislike".equalsIgnoreCase(text)) {
-                chatStates.put(chatId, ChatState.NO_POST);
-                sendMessage(chatId, "Предстааавьте, что мы записал дизлайк");
-            } else if ("next".equalsIgnoreCase(text)) {
-                sendMessage(chatId, "Предстааавьте, что это пост с новостями");
-            } else if ("add source".equalsIgnoreCase(text)) {
-                chatStates.put(chatId, ChatState.AWAITING_URI);
-                sendMessage(chatId, "Введите URI нового источника: ");
-            } else {
+            } else if (!handleNormalState(chatId, state, text)) {
                 sendMessage(chatId, "Операция " + text + " не поддерживается");
             }
         } catch (TelegramApiException e) {
             throw new RuntimeException(e);
         }
     }
-
-    @Override
-    public String getBotUsername() {
-        return "HsseNewsTeam1Bot";
-    }
-
-    private static KeyboardRow getCommandsRow() {
-        KeyboardRow commandsRow = new KeyboardRow();
-        commandsRow.add("next");
-        commandsRow.add("add source");
-        return commandsRow;
-    }
-
-    private static ReplyKeyboard getPostKeyboard() {
-        KeyboardRow likeRow = new KeyboardRow();
-        likeRow.add("like");
-        likeRow.add("dislike");
-        return new ReplyKeyboardMarkup(List.of(likeRow, getCommandsRow()));
-    }
-
-    private static ReplyKeyboard getNoPostKeyboard() {
-        return new ReplyKeyboardMarkup(List.of(getCommandsRow()));
-    }
-
-    private void sendMessage(long chatId, String text) throws TelegramApiException {
-        SendMessage message = new SendMessage();
-        message.setChatId(chatId);
-
-        message.setText(text);
-
-        if (chatStates.get(chatId).equals(ChatState.NO_POST)) {
-            message.setReplyMarkup(getNoPostKeyboard());
-        } else if (chatStates.get(chatId).equals(ChatState.POST)) {
-            message.setReplyMarkup(getPostKeyboard());
-        }
-
-        execute(message);
-    }
-
 }
