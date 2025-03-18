3232f6de59779dcac1cb1b1a41ae2a03ec62545e
PJutch <103033969+PJutch@users.noreply.github.com>
2025-03-18 14:25:51 +0300

    Add some operations to TopicService

--- app/src/main/java/org/hsse/news/api/schemas/request/topic/CreateCustomTopicRequest.java
+++ app/src/main/java/org/hsse/news/api/schemas/request/topic/CreateCustomTopicRequest.java
@@ -1,8 +1,9 @@
 package org.hsse.news.api.schemas.request.topic;
 
+import org.hsse.news.database.user.models.UserId;
 import org.jetbrains.annotations.NotNull;
 
 public record CreateCustomTopicRequest(
-        @NotNull String description
+        @NotNull String name, @NotNull UserId creatorId
 ) {
 }
--- app/src/main/java/org/hsse/news/database/topic/TopicService.java
+++ app/src/main/java/org/hsse/news/database/topic/TopicService.java
@@ -1,11 +1,48 @@
 package org.hsse.news.database.topic;
 
+import lombok.AllArgsConstructor;
+import org.hsse.news.api.schemas.request.topic.CreateCustomTopicRequest;
+import org.hsse.news.database.topic.models.Topic;
+import org.hsse.news.database.topic.models.TopicDto;
 import org.hsse.news.database.topic.models.TopicId;
+import org.hsse.news.database.topic.repositories.TopicRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
+@AllArgsConstructor
 public class TopicService {
-    public String getTopicNameById(final TopicId id) {
-        return id.toString();
+    private final TopicRepository repository;
+
+    @Transactional
+    public List<TopicDto> getAll() {
+        return repository.findAll().stream().map(Topic::toDto).toList();
+    }
+
+    @Transactional
+    public Optional<TopicDto> getById(final TopicId id) {
+        return repository.findById(id.value()).map(Topic::toDto);
+    }
+
+    @Transactional
+    public void create(CreateCustomTopicRequest data) {
+        repository.save(new Topic(data.name(), data.creatorId()));
+    }
+
+    @Transactional
+    public void update(final TopicId id, CreateCustomTopicRequest data) {
+        repository.findById(id.value()).ifPresent((topic) -> {
+            topic.setName(data.name());
+            topic.setCreatorId(data.creatorId().value());
+            repository.save(topic);
+        });
+    }
+
+    @Transactional
+    public void delete(final TopicId id) {
+        repository.deleteById(id.value());
     }
 }
