64e488de96bc0779738cd6a488982f1c274ea7e2
PJutch <103033969+PJutch@users.noreply.github.com>
2025-04-29 23:48:24 +0300

    Add more default websites

---
+++ app/src/main/resources/db/migration/V7__more_websites.sql
@@ -0,0 +1,10 @@
+INSERT INTO websites (url, description) VALUES
+    ('https://blog.algomaster.io/t/system-design', 'System Design Interviews from AlgoMaster Newsletter'),
+    ('https://news.apache.org/', 'The Apache Software Foundation Blog'),
+    ('https://www.decodable.co/blog', 'Decodable, the real-time data platform powered by Apache Flink and Debezium'),
+    ('https://fingerprint.com/blog/', 'Fingerprint, visitor identifier'),
+    ('https://flink.apache.org/posts/', 'Apache Flink®, Stateful Computations over Data Streams framework'),
+    ('https://kafka.apache.org/blog', 'Apache Kafka, open-source distributed event streaming platform'),
+    ('https://www.atomicjar.com/category/testcontainers/', 'AtomicJar (Testcontainers and Testcontainers Cloud)'),
+    ('https://vladmihalcea.com/blog/', 'Vlad Mihalcea, author of High-Performance Java Persistence book'),
+    ('https://webkit.org/blog/category/news/', 'Webkit, a fast, open source web browser engine.')
